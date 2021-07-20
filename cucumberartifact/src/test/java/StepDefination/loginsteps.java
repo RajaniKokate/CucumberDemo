be028a41eb7e09225884cf9a0ad64d593e8637fc
0017WP744 <0017WP744@9.199.55.46>
2021-07-20 09:25:59 +0530

    comitting project

---
+++ cucumberartifact/src/test/java/StepDefination/loginsteps.java
@@ -0,0 +1,24 @@
+package StepDefination;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+
+public class loginsteps {
+	
+	WebDriver driver;
+	@Given("^I am on Login page$")
+	public void loginpage()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\0017WP744\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        //i am trying to create a new instance of chrome driver
+        driver = new ChromeDriver();
+        //WedDriver driver - new ChromeDriver()
+        //ChromeDriver driver = new ChromeDriver()
+        //i want to be the login page //what should i do
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+	System.out.println("I am on login page");
+	}
+
+}
---
+++ cucumberartifact/src/test/resources/Login/test.feature
@@ -0,0 +1,3 @@
+Feature: To validate login application
+Scenario: To test login application with valid conditions
+Given I am on Login page
